b5478727d822d4b1d4b3912f842892507a3938ca
gvincent <GaryVincent@quickenloans.com>
2018-07-26 12:20:27 -0400

    swipe controller for attendance rv

--- mobile/src/main/java/fragments/Attendance.kt
+++ mobile/src/main/java/fragments/Attendance.kt
@@ -1,10 +1,12 @@
 package fragments
 
 import activities.MainActivity
+import android.graphics.Canvas
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +16,10 @@ import com.vince.childcare.R
 import kotlinx.android.synthetic.main.fragment_attendance.view.*
 
 
+
+
 class Attendance : Fragment() {
+  private var swipeController: SwipeController? = null
 
   var children: ArrayList<AttenChild> = ArrayList()
   lateinit var rv: RecyclerView
@@ -47,9 +52,33 @@ class Attendance : Fragment() {
   }
 
   private fun setupRecyclerView(view: View) {
+
+
     rv = view.attendance_rv as RecyclerView
     rv.layoutManager = LinearLayoutManager(this.context)
     adapter = AttendanceAdapter(children, this.context!!)
     rv.adapter = adapter
+
+
+    swipeController = SwipeController(object : SwipeControllerActions() {
+      override fun onRightClicked(position: Int) {
+        //todo - delete child/data associated from firestore
+          //todo - get fresh data from firestore on completion of previous
+            //todo - update recyclerview data on completion of previous
+      }
+      override fun onLeftClicked(position: Int) {
+        //todo - get child document/associated data from firestore
+          //todo - open registration activity with data for this child
+      }
+    })
+
+    val itemTouchhelper = ItemTouchHelper(swipeController)
+    itemTouchhelper.attachToRecyclerView(rv)
+
+    rv.addItemDecoration(object : RecyclerView.ItemDecoration() {
+      override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        swipeController!!.onDraw(c)
+      }
+    })
   }
 }
